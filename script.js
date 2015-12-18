141c0002a477e34adb8981a43ff10fa940b3f2a8
David King <david@oodavid.com>
2015-12-18 17:46:08 +0000

    tweaked todo

--- script.js
+++ script.js
@@ -19,6 +19,10 @@
 		log('todo: tap to create a dot fails when using the ALT modifier');
 		log('todo: automatically upload to youtube');
 		log('todo: get a better SSL certificate from letsencrypt.org');
+		log('todo: canvas panning');
+		log('todo: cut, copy, paste');
+		log('todo: eraser');
+		log('todo: image upload - maybe a \'clipboard\' sidebar that the user can pre-populate?');
 		log('---------')
 	});
 	// Color selection
